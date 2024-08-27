14a8aa552d54a3a36c160c0d120f9551ee310900
serkan-can-eyvaz <serkancaneyvaz@gmail.com>
2024-08-27 15:23:13 +0300

    project created

---
+++ src/main/java/com/example/runningperformance/RunningPerformanceApplication.java
@@ -0,0 +1,13 @@
+package com.example.runningperformance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunningPerformanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RunningPerformanceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/runningperformance/RunningPerformanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.runningperformance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunningPerformanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
