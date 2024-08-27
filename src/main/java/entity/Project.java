7e65e85e003991c1e2dde0467f9cd7ef927e65df
serkan-can-eyvaz <serkancaneyvaz@gmail.com>
2024-08-27 19:35:39 +0300

    repository completed service package add

--- src/main/java/entity/Project.java
+++ src/main/java/entity/Project.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Objects;
 
 @Entity
+@Table(name = "Project")
 public class Project {
 
     @Id
--- src/main/java/entity/Task.java
+++ src/main/java/entity/Task.java
@@ -6,6 +6,7 @@ import java.util.Date;
 import java.util.Objects;
 
 @Entity
+@Table(name = "Task")
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
